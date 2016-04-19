39309d41c3260e8644b7657cf80476e775f24c25
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-04-19 21:09:56 +0100

    04 Views » Console

---
+++ Assets/LogMessages.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogMessages : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World!");
+        Debug.Log("Hello World 2");
+        Debug.LogWarning("Hello Warn");
+        Debug.LogError("Hello Error");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
